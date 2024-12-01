ee75dea25499ff4acfb75973ba6764aac1ebf3f2
Sally Mohamed <sally.mohamed@digital.trade.gov.uk>
2024-12-01 19:30:13 +0000

    Added part 2 solution and small refactor of part 1

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 with open("puzzle_input.txt") as f:
     list_1=[]
     list_2=[]
@@ -7,10 +9,7 @@ with open("puzzle_input.txt") as f:
         list_1.append(int(line_list[0]))
         list_2.append(int(line_list[-1]))
 
-sorted_list_1 = sorted(list_1)
-sorted_list_2 = sorted(list_2)
-
-zipped_lists = list(zip(sorted_list_1,sorted_list_2))
+zipped_lists = list(zip(sorted(list_1),sorted(list_2)))
 
 differences = []
 for pair in zipped_lists:
@@ -18,4 +17,12 @@ for pair in zipped_lists:
     differences.append(difference)
 
 p1_answer = sum(differences)
-print(p1_answer)
+print(f"Answer to part 1 is: {p1_answer}")
+
+similarity_scores = []
+for i in list_1:
+    similarity_score = i*Counter(list_2)[i]
+    similarity_scores.append(similarity_score)
+
+p2_answer = sum(similarity_scores)
+print(f"Answer to part 2 is: {p2_answer}")

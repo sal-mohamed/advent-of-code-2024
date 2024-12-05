fb7ca3713db898e9d7597f5496bf96442a815230
Sally Mohamed <sally.mohamed@digital.trade.gov.uk>
2024-12-05 14:32:43 +0000

    Day 5 part 2

--- day_5/solution.py
+++ day_5/solution.py
@@ -14,7 +14,6 @@ with open("puzzle_page_order.txt") as f:
         rules[first].add(second)
 
 with open("puzzle_update.txt") as f:
-    #update_data = [list(map(int,(line.rstrip()).split(","))) for line in f]
     update_data = [(line.rstrip()).split(",") for line in f]
 
 invalid_updates = []
@@ -30,9 +29,20 @@ for update in update_data:
                     invalid_updates.append(update)
 
 invalid_updates = (list(invalid_updates for invalid_updates,_ in groupby(invalid_updates)))
-
 valid_updates = [update for update in update_data if update not in invalid_updates]
-
 p1_answer = sum([find_middle(i) for i in valid_updates])
-
 print(f"Answer to p1 is {p1_answer}")
+
+sorted_pages=[]
+for idx,update in enumerate(invalid_updates):
+    valid = False
+    page = update
+    n = len(page)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if str(page[j+1]) in rules[str(page[j])]:
+                page[j], page[j+1] = page[j+1], page[j]
+    sorted_pages.append(page)
+
+p2_answer = sum([find_middle(i) for i in sorted_pages])
+print(f"Answer to p2 is {p2_answer}")
